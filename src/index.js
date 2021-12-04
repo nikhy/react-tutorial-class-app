89c6b8584ee74a89e4bf2a2ce70e260c92895629
Nikhythan C <nikhythanc@gmail.com>
2021-12-04 22:50:42 +0530

    Declaring a Winner

--- src/index.js
+++ src/index.js
@@ -21,8 +21,30 @@ function Square(props) {
             XIsNext: true
         };
     }
+    calculateWinner(squares){
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+        for ( let i = 0; i < lines.length; i++ ){
+            const [a, b, c] = lines[i];
+            if ( squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+                return squares[a];
+            }
+        }
+        return null;
+    }
     handleClick(i){
         const squares = this.state.squares.slice();
+        if (this.calculateWinner(squares) || squares[i]){
+            return;
+        }
         squares[i] =  this.state.XIsNext ? 'X' : 'O';
         this.setState({
             squares : squares,
@@ -37,7 +59,14 @@ function Square(props) {
     }
   
     render() {
-      const status = 'Next player: ' + (this.state.XIsNext ? 'X' : 'O' );
+      
+      const winner = this.calculateWinner(this.state.squares);
+      let status;
+      if (winner){
+          status = 'Winner: ' + winner;
+      } else {
+          status = 'Next player: ' + (this.state.XIsNext ? 'X' : 'O' );
+      }
   
       return (
         <div>
